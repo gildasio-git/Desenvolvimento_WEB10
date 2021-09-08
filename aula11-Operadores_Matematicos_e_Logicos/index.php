79d011909c49aab93296af906e118b9287c80bde
Gildasio Bizerra Brito <gildasio.ariquemes@hotmail.com>
2021-09-08 01:29:03 -0400

    Operadores Lógico e Matemáticos

---
+++ aula11-Operadores_Matematicos_e_Logicos/index.php
@@ -0,0 +1,45 @@
+<?php
+/*
+
+    $numero1 =10;
+    $numero2 =11; //Funciona se for string também 
+
+     //Operadores matemáticos
+     if($numero1 < $numero2){
+
+     }else{
+
+     }
+
+
+     if($numero1 > $numero2){
+
+     }else{
+
+     }
+
+     if($numero1 >= $numero2){
+         echo 'execute';
+     }
+*/
+
+
+/*
+//Operadores Lógicos
+
+$var1 = 'valor1';
+$var2 = 'valor2';
+$var3 = 'valor3';
+
+//Operadores lógico  'E'
+if(($var1 == $var2) && ($var2 == $var3)){
+   echo 'verdade';
+}
+
+//Operador lógico 'OU'
+if(($var1 == $var2) || ($var1 == $var3) && $var1 == $var2){
+    echo 'falso 
+}
+*/
+
+?>
