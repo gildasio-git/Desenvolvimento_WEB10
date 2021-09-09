19dd5a81756d55d0daac287eab8eebc3484fc216
Gildasio Bizerra Brito <gildasio.ariquemes@hotmail.com>
2021-09-08 23:25:47 -0400

    Inclusão anotações aula 12 e 13

---
+++ aula12-Loopings/index.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+    //Laço de repetição FOR 
+    for($contador=0$contador<10;$contador++ ){
+        echo 'Ola mundo ';
+        echo </br>;
+        echo $contador;
+    }
+*/
+
+/*
+    //Laço de repetição WHILE - precisamos declara a variável antes
+    $cont = 0;
+    while ($cont < 10) { //Testa essa linha condicional antes de executar o loop
+        # code...
+        echo 'alo mundo';      
+        echo '<hr>';
+        $cont++;
+    }
+*/
+
+
+/*
+//Laço de repetição DO
+$cont=0;
+
+do {
+    # code...
+    echo 'ola mundo '; //Executa pelo menos uma vez o teste antes de testar a condição 
+    echo '<br/>';
+    $cont++;
+
+}while ($cont = 10);
+
+*/
+
+?>
---
+++ aula13-Comentários_no_Código/index..php
@@ -0,0 +1,18 @@
+<?
+    //COMENTÁRIOS NO CÓDIGO 
+
+    //FORMA 1
+    //Variável nome
+    $nome = "gildasio";
+
+    //FORMA II
+    /**
+     * Todo esse espaço reservado 
+     * aos comentários do programa
+     * dedicado a textos mais longos. 
+     * 
+     */
+
+     //FORMA III
+     #USANDO JOGO DA VELHA 
+?>
